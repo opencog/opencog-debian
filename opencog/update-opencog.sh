a28a067093786d2063d10901ba3e24776e7e3604
Masayuki Hatta <mhatta@debian.org>
2019-08-22 13:40:23 -0700

    Add visualization

--- opencog/update-opencog.sh
+++ opencog/update-opencog.sh
@@ -23,3 +23,4 @@ cp -R ../opencog-debian/opencog/debian $DEB_DIR_NAME
 
 cd $DEB_DIR_NAME && dch -v $VERSION~git$DATE.$GIT_HASH-1 && cd ..
 
+cp $DEB_DIR_NAME/debian/changelog ../opencog-debian/$REPO_NAME/debian
--- visualization/update-visualization.sh
+++ visualization/update-visualization.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-REPO_NAME=relex
-DEB_NAME=opencog-relex
+REPO_NAME=visualization
+DEB_NAME=opencog-visualization
 
-# You can find this in README.md
-VERSION=1.6.3
+VERSION=0.0
 
 DATE=`date "+%Y%m%d"`
 GIT_HASH=`cd $REPO_NAME && git rev-parse --short=7 HEAD`
 DEB_DIR_NAME=$DEB_NAME-$VERSION~git$DATE.$GIT_HASH
 DEB_ARCHIVE_NAME=$DEB_NAME\_$VERSION~git$DATE.$GIT_HASH
+
+rm -rf ~/.cache/guile
 
 cd $REPO_NAME && git fetch upstream && git merge upstream/master && cd ..
 cp -R $REPO_NAME $DEB_DIR_NAME
@@ -18,8 +19,9 @@ cd $DEB_DIR_NAME && rm -rf .git && find . -type f -name .gitignore -exec rm {} \
 
 tar -cvjSf $DEB_ARCHIVE_NAME.orig.tar.bz2 $DEB_DIR_NAME
 
-cp -R ../opencog-debian/relex/debian $DEB_DIR_NAME
+cp -R ../opencog-debian/visualization/debian $DEB_DIR_NAME
 
 cd $DEB_DIR_NAME && dch -v $VERSION~git$DATE.$GIT_HASH-1 && cd ..
 
 cp $DEB_DIR_NAME/debian/changelog ../opencog-debian/$REPO_NAME/debian
+
