1910632944fcdb9b1bcb8764d46d1e8f15f26d5f
Masayuki Hatta <mhatta@gnu.org>
2020-04-02 22:49:47 +0900

    Updated

--- relex/update-relex.sh
+++ relex/update-relex.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 REPO_NAME=relex
@@ -11,15 +12,15 @@ GIT_HASH=`cd $REPO_NAME && git rev-parse --short=7 HEAD`
 DEB_DIR_NAME=$DEB_NAME-$VERSION~git$DATE.$GIT_HASH
 DEB_ARCHIVE_NAME=$DEB_NAME\_$VERSION~git$DATE.$GIT_HASH
 
-cd $REPO_NAME && git fetch upstream && git merge upstream/master && cd ..
+cd $REPO_NAME && git fetch upstream && git merge upstream/master && git push && cd ..
 cp -R $REPO_NAME $DEB_DIR_NAME
 
 cd $DEB_DIR_NAME && rm -rf .git && find . -type f -name .gitignore -exec rm {} \; && cd ..
 
-tar -cvjSf $DEB_ARCHIVE_NAME.orig.tar.bz2 $DEB_DIR_NAME
+tar -cJf $DEB_ARCHIVE_NAME.orig.tar.xz $DEB_DIR_NAME
 
-cp -R ../opencog-debian/relex/debian $DEB_DIR_NAME
+cp -R ../opencog-debian/$REPO_NAME/debian $DEB_DIR_NAME
 
-cd $DEB_DIR_NAME && dch -v $VERSION~git$DATE.$GIT_HASH-1 && cd ..
+cd $DEB_DIR_NAME && dch -D mhatta-unstable --force-distribution -v $VERSION~git$DATE.$GIT_HASH-1 "New upstream snapshot." && cd ..
 
 cp $DEB_DIR_NAME/debian/changelog ../opencog-debian/$REPO_NAME/debian
